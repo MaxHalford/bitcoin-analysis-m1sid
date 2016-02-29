7316106fa3d330a3b23e7010715ddfe103b14d5f
Max Halford <maxhalford25@gmail.com>
2016-02-29 15:02:17 +0100

    Rangement

--- profit/bd_insertions.py
+++ profit/bd_insertions.py
@@ -19,9 +19,9 @@ session.commit()
 # A finir...
 
 # Prix de l'électricité
-electricite = pd.read_csv('data/electricite.csv')
+electricite = pd.read_csv('../data/electricite.csv')
 electricite.index = pd.to_datetime(electricite.periode, format='%Y_%m')
 electricite.drop('periode', axis=1, inplace=True)
 
 # Machines
-machines = pd.read_csv('data/machines.csv', index_col='nom')
+machines = pd.read_csv('../data/machines.csv', index_col='nom')
