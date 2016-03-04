a32672ba67ef9b60bf09e52e53d12f4465f46642
Max Halford <maxhalford25@gmail.com>
2016-03-04 15:34:00 +0100

    BD créées

--- datawarehouse/bd_creation.py
+++ datawarehouse/bd_creation.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, DateTime, Integer, Float, String ForeignKey
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
 
 Base = declarative_base()
 
@@ -8,69 +8,72 @@ Base = declarative_base()
 class Utilisateur(Base):
     __tablename__ = 'utilisateurs'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
+    id = Column(Integer, autoincrement=True, primary_key=True)
     nom = Column(String)
 
 
 class Tweet(Base):
     __tablename__ = 'tweets'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
+    id = Column(Integer, autoincrement=True, primary_key=True)
     contenu = Column(String)
-    utilisateur = Column(String, db.ForeignKey('utilisateurs.id'))
-    date = Column(String, db.ForeignKey('dates.id'))
+    likes = Column(Integer)
+    retweets = Column(Integer)
+    sentiment = Column(String)
+    utilisateur = Column(String, ForeignKey('utilisateurs.id'))
+    date = Column(String, ForeignKey('dates.id'))
 
 
 class Date(Base):
     __tablename__ = 'dates'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
-    date = Column(Datetime)
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    date = Column(DateTime)
 
 
 class Terme(Base):
     __tablename__ = 'termes'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
+    id = Column(Integer, autoincrement=True, primary_key=True)
     terme = Column(String)
 
 
 class Url(Base):
     __tablename__ = 'urls'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
+    id = Column(Integer, autoincrement=True, primary_key=True)
     url = Column(String)
 
 
 class Hashtag(Base):
-    __tablename__ = 'urls'
+    __tablename__ = 'hashtags'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
+    id = Column(Integer, autoincrement=True, primary_key=True)
     hashtag = Column(String)
 
 
 class TermeDansTweet(Base):
     __tablename__ = 'terme_dans_tweets'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
-    terme = Column(Integer, db.ForeignKey('termes.id'))
-    tweet = Column(Integer, db.ForeignKey('tweets.id'))
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    terme = Column(Integer, ForeignKey('termes.id'))
+    tweet = Column(Integer, ForeignKey('tweets.id'))
 
 
 class UrlDansTweet(Base):
     __tablename__ = 'url_dans_tweet'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
-    url = Column(Integer, db.ForeignKey('urls.id'))
-    tweet = Column(Integer, db.ForeignKey('tweets.id'))
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    url = Column(Integer, ForeignKey('urls.id'))
+    tweet = Column(Integer, ForeignKey('tweets.id'))
 
 
 class HashtagDansTweet(Base):
     __tablename__ = 'hashtag_dans_tweet'
 
-    id = Column(Integer, autoincrement=True, primary_key=True))
-    hashtag = Column(Integer, db.ForeignKey('hashtags.id'))
-    tweet = Column(Integer, db.ForeignKey('tweets.id'))
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    hashtag = Column(Integer, ForeignKey('hashtags.id'))
+    tweet = Column(Integer, ForeignKey('tweets.id'))
 
 
 engine = create_engine('sqlite:///tweets.db')
--- datawarehouse/bd_insertions.py
+++ datawarehouse/bd_insertions.py
@@ -1,3 +1,91 @@
 import pandas as pd
+import datetime
+import re
+from urllib.parse import urlparse
+from sqlalchemy.orm import sessionmaker
+from bd_creation import engine
+from bd_creation import (Utilisateur, Tweet, Date, Terme, Url, Hashtag,
+                         TermeDansTweet, UrlDansTweet, HashtagDansTweet)
 
+session = sessionmaker(bind=engine)()
 df = pd.read_csv('../data/dataframes/allTweets.csv')
+
+# On vide les tables
+session.query(Utilisateur).delete()
+session.query(Tweet).delete()
+session.query(Date).delete()
+session.query(Terme).delete()
+session.query(Url).delete()
+session.query(Hashtag).delete()
+session.query(TermeDansTweet).delete()
+session.query(UrlDansTweet).delete()
+session.query(HashtagDansTweet).delete()
+
+for username in df['username'].unique():
+    session.add(Utilisateur(nom=username))
+session.commit()
+
+for ts in df['timestamp'].unique():
+    session.add(Date(date=datetime.datetime.fromtimestamp(ts / 1000)))
+session.commit()
+
+T = []
+H = []
+U = []
+
+
+def get_info_from_tweet(tweet):
+    hashtags = set()
+    urls = set()
+    termes = set()
+    for elem in re.split('\s', tweet):
+        # Hashtag
+        if elem.startswith('#'):
+            hashtags.add(elem)
+        # Lien
+        elif elem.startswith('http'):
+            host = urlparse(elem).hostname
+            urls.add(host)
+        # Terme
+        else:
+            termes.add(elem)
+    return (termes, hashtags, urls)
+
+
+def add(row):
+    date = datetime.datetime.fromtimestamp(row['timestamp'] / 1000)
+    tweet = Tweet(
+        contenu=row['tweet'],
+        likes=row['likes'],
+        retweets=row['retweets'],
+        sentiment=row['sentiment'],
+        utilisateur=row['username'],
+        date=date
+    )
+    session.add(tweet)
+    session.commit()
+    tweet_id = tweet.id
+    termes, hashtags, urls = get_info_from_tweet(tweet.contenu)
+    for terme in termes:
+        session.add(TermeDansTweet(tweet=tweet_id, terme=terme))
+        session.commit()
+        if terme not in T:
+            session.add(Terme(terme=terme))
+            session.commit()
+            T.append(terme)
+    for hashtag in hashtags:
+        session.add(HashtagDansTweet(tweet=tweet_id, hashtag=hashtag))
+        session.commit()
+        if hashtag not in H:
+            session.add(Hashtag(hashtag=hashtag))
+            session.commit()
+            H.append(hashtag)
+    for url in urls:
+        session.add(UrlDansTweet(tweet=tweet_id, url=url))
+        session.commit()
+        if url not in U:
+            session.add(Url(url=url))
+            session.commit()
+            U.append(url)
+
+df.apply(lambda row: add(row), axis=1)
